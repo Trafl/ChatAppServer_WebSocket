dbf61cb22b05ace22eeb43152bd36d26d1257cb7
P.Ivo <pivolluz@gmail.com>
2024-05-30 20:29:11 -0300

    Adicionado logs no ChatController

--- src/main/java/com/trafl/chat/controller/ChatController.java
+++ src/main/java/com/trafl/chat/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.trafl.chat.controller;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -14,12 +15,20 @@ import com.trafl.chat.domain.Msg;
 import com.trafl.chat.domain.MsgAddUserDTO;
 import com.trafl.chat.domain.MsgDTO;
 
+import lombok.extern.log4j.Log4j2;
+
 @Controller
+@Log4j2
 public class ChatController {
+	
+	String timestamp = LocalDateTime.now().toString();
 
 	@MessageMapping("/chat_send_message") // Manda msg no /app/chat_send_message
 	@SendTo("/chat") // A mensagem e encaminhada ao /chat
 	public Msg sendMessage(@Payload MsgDTO messageDTO) {
+		
+		log.info("[{}] - [ChatController] - Message received", timestamp);
+		
 		String time = createDateInStringFormat();
 		
 		var message = Msg.builder()
@@ -29,6 +38,7 @@ public class ChatController {
 						.type(messageDTO.type())
 						.build();
 		
+		log.info("[{}] - [ChatController] - Message Build and send", timestamp);
 		return message;
 	}
 	
@@ -37,7 +47,7 @@ public class ChatController {
 	 public MsgAddUserDTO addUser(@Payload MsgAddUserDTO messageDTO, SimpMessageHeaderAccessor headerAccessor) {
 		// add username to session
 		headerAccessor.getSessionAttributes().put("username", messageDTO.name());
-		
+		log.info("[{}] - [ChatController] - username: {}, add to headerAccessor", timestamp, messageDTO.name());
         return messageDTO;
 	}
 
