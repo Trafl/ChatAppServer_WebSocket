9f6227af2c40aace17459e84c8db418151f2e10b
P.Ivo <pivolluz@gmail.com>
2024-05-30 20:32:41 -0300

    Adicionando Logs ao EventListiner

--- src/main/java/com/trafl/chat/config/WebSocketEventListener.java
+++ src/main/java/com/trafl/chat/config/WebSocketEventListener.java
@@ -1,5 +1,7 @@
 package com.trafl.chat.config;
 
+import java.time.LocalDateTime;
+
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
@@ -17,6 +19,8 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class WebSocketEventListener {
 	
+	String timestamp = LocalDateTime.now().toString();
+	
 	private final SimpMessageSendingOperations messageSendingOperations;
 
 	@EventListener
@@ -27,7 +31,7 @@ public class WebSocketEventListener {
 		String username = (String) headerAccessor.getSessionAttributes().get("username");
         
 		if (username != null) {
-            log.info("user disconnected: {}", username);
+            log.info("[{}] - [WebSocketEventListener] - user disconnected: {}",timestamp, username);
             var chatMessage = Msg.builder()
                     .type(MsgType.LEAVER)
                     .username(username)
